5f99be25da92a1bc5810c17e20465f400545ce81
eneskarateke <eneskarateke@gmail.com>
2023-05-23 18:51:45 +0300

    esnekler hariçı

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
 
 import { Route, Switch, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
 
-import MovieHeader from './components/MovieHeader';
+import MovieHeader from "./components/MovieHeader";
 
-import AddMovieForm from './components/AddMovieForm';
-import FavoriteMovieList from './components/FavoriteMovieList';
+import AddMovieForm from "./components/AddMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-const App = props => {
-  const displayFavorites = true;
+const App = (props) => {
+  // const displayFavorites = true;
+  const displayFavorites = useSelector(
+    (store) => store.favoritesReducer.displayFavorites
+  );
 
   return (
     <div>
--- src/components/FavoriteMovieList.js
+++ src/components/FavoriteMovieList.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { removeFavorite } from "../actions/favoritesActions";
 
 const FavoriteMovieList = (props) => {
-  const favorites = [];
+  const dispatch = useDispatch();
+  const { push } = useHistory();
+  // const { id } = useParams();
+
+  // const favorites = [];
+  const favorites = useSelector((store) => store.favoritesReducer.favorites);
+  // const movie = favorites.find((movie) => movie.id === Number(id));
+
+  const handleRemoveFavorite = (id) => {
+    dispatch(removeFavorite(id));
+    // push("/movies");
+  };
 
   return (
     <div className="flex-1 sm:max-w-[250px] p-5 pr-5 bg-white shadow rounded-md">
-      <h5 className='font-bold'>Favori Filmler</h5>
+      <h5 className="font-bold">Favori Filmler</h5>
       <div className="pt-3 text-sm">
-        {
-          favorites.map(movie => (
-            <Link key={movie.id} className="py-1 flex gap-2 justify-between" to={`/movies/${movie.id}`}>
-              {movie.title}
-              <span className="material-icons hover:text-red-600 text-[18px]">remove_circle</span>
-            </Link>
-          ))
-        }
+        {favorites.map((movie) => (
+          <Link
+            key={movie.id}
+            className="py-1 flex gap-2 justify-between"
+            to={`/movies/${movie.id}`}
+          >
+            {movie.title}
+            <span
+              onClick={() => handleRemoveFavorite(movie.id)}
+              className="material-icons hover:text-red-600 text-[18px]"
+            >
+              remove_circle
+            </span>
+          </Link>
+        ))}
       </div>
     </div>
   );
-}
-
+};
 
 export default FavoriteMovieList;
